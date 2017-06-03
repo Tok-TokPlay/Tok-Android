0d029b8689a2f856c92276be0d747c4521557d40
최현준 <chj878194@naver.com>
2017-06-03 23:06:08 +0900

    Data class modified
    Data class modified

--- android_tok/TokTokPlay/app/src/main/java/com/example/dkdk6/toktokplay/Activity/Data.java
+++ android_tok/TokTokPlay/app/src/main/java/com/example/dkdk6/toktokplay/Activity/Data.java
@@ -1,22 +1,29 @@
 package com.example.dkdk6.toktokplay.Activity;
 
-/**
- * Created by dkdk6 on 2017-06-01.
- */
-
 import java.io.Serializable;
 import java.util.ArrayList;
 
-/**
- * Created by 4p on 2017-06-01. 박진희
- */
-
 public class Data implements Serializable {
-    ArrayList<Integer> userBeat = new ArrayList<>();
-    public ArrayList getList(){
-        return userBeat;
+	private static final long serialVersionUID = 878194L;
+	//serialVersionUID is for check serialize ID.
+	//This may help flexible checking to Old and new version.
+	private ArrayList<Integer> list = new ArrayList<>();
+	
+	public ArrayList<Integer> getList(){
+        return list;
     }
+	
     public void setList(ArrayList<Integer> list){
-        this.userBeat=list;
+        this.list=list;
+    }
+    
+    @Override
+    public String toString()	{
+		//Change Data list into String.
+    	String returnValue = "";
+    	for(int i = 0; i < this.getList().size(); i++){
+    		returnValue = returnValue.concat(getList().get(i).toString());
+    	}
+    	return returnValue;
     }
 }
